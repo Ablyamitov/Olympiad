8460d007813755ed521ee3451a4df2b6d858a2ae
Ablyamitov <ablyamitov2003@mail.ru>
2024-11-29 14:10:04 +0300

    add "Access-Control-Allow-Credentials", "true"

--- src/main/java/com/cfuv/olympus/web/security/CORSFilter.java
+++ src/main/java/com/cfuv/olympus/web/security/CORSFilter.java
@@ -56,7 +56,7 @@ public class CORSFilter implements Filter {
 //            response.setHeader("Access-Control-Allow-Origin", origin);
 //            response.setHeader("Access-Control-Allow-Credentials", "true");
 //        }
-
+        response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Headers", "Date, Content-Type, Accept, X-Requested-With, Authorization, From, X-Auth-Token, Request-Id, ngrok-skip-browser-warning");
         response.setHeader("Access-Control-Expose-Headers", "Set-Cookie");
